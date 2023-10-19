614e0d5b502800270a15d47b270edcb5c5f2c08f
Sunny Hirai <thesunny@gmail.com>
2023-10-19 00:53:44 -0700

    Fix lint issues

--- apps/web/pages/[name].tsx
+++ apps/web/pages/[name].tsx
@@ -1,44 +1,15 @@
 import fs from "fs"
-import path from "path"
-
-import ckeditorData from "../../../data/editors/@ckeditor--ckeditor5-engine.json"
-import editorjsData from "../../../data/editors/@editorjs--editorjs.json"
-import tiptapData from "../../../data/editors/@tiptap--core.json"
-import draftData from "../../../data/editors/draft-js.json"
-import froalaData from "../../../data/editors/froala-editor.json"
-import prosemirrorMarkdown from "../../../data/editors/prosemirror-markdown.json"
-import prosemirror from "../../../data/editors/prosemirror-model.json"
-import quill from "../../../data/editors/quill.json"
-import slate from "../../../data/editors/slate.json"
-import summernote from "../../../data/editors/summernote.json"
-import tinymce from "../../../data/editors/tinymce.json"
-import trix from "../../../data/editors/trix.json"
-import { DownloadsChart, PackageData } from "../components/DownloadsChart"
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next"
 import Link from "next/link"
+import path from "path"
 
-// const data = [
-//   ckeditorData,
-//   editorjsData,
-//   tiptapData,
-//   draftData,
-//   froalaData,
-//   prosemirror,
-//   prosemirrorMarkdown,
-//   quill,
-//   slate,
-//   summernote,
-//   tinymce,
-//   trix,
-// ]
+import { DownloadsChart, PackageData } from "../components/DownloadsChart"
 
 const secondaryPackageNames = [
   "@ckeditor/ckeditor5-engine",
   "@tiptap/core",
   "tinymce",
 ]
-
-// {"start":"2022-10-05","end":"2023-10-04","package":"blueimp-file-upload","downloads":[{"downloads":14751,"day":"2022-10-05"},{"downloads":15000,"day":"2022-10-0
 
 export function getServerSideProps(context: GetServerSidePropsContext) {
   const subdir = context.params?.name as string
@@ -50,7 +21,6 @@ export function getServerSideProps(context: GetServerSidePropsContext) {
     const parsedData = JSON.parse(fileData)
     return parsedData as PackageData
   })
-  console.log(data)
 
   return {
     props: {
